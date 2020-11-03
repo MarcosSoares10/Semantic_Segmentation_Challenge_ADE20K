0e20a49029283613d5fed4ba86e83c1813a4291f
MarcosSoares10 <marquinhoscamaleao@gmail.com>
2020-11-03 00:15:10 -0300

    Update SemanticSegmentation.py

--- SemanticSegmentation.py
+++ SemanticSegmentation.py
@@ -2,65 +2,105 @@
 #http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
 
 
-from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, MaxPool2D, UpSampling2D, Concatenate,concatenate,MaxPooling2D
-from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
-import numpy as np
-from sklearn.model_selection import train_test_split
-import cv2
 import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, MaxPooling2D, UpSampling2D, Concatenate, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
+import numpy as np
+import cv2
 from glob import glob
-import datetime
+from time import time
 
-#import os
-#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
-#os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 
 IMG_SIZE = 128
 N_CHANNELS = 3
 N_CLASSES = 151
-batch_size = 60
+batch_size = 8
 
-def createConvolutionBlock(inputs, filters, pool = True):
+
+def createConvolutionBlock(inputs, filters, pool=False, dropout=False, dilation=False):
     initializer = 'he_normal'
-    convA = Conv2D(filters, 3, padding="same",kernel_initializer=initializer)(inputs)
-    batchA = BatchNormalization()(convA)
-    actvA = Activation("relu")(batchA)
 
-    convB = Conv2D(filters, 3, padding="same",kernel_initializer=initializer)(actvA)
-    batchB = BatchNormalization()(convB)
-    actvB = Activation("relu")(batchB)
 
-    if pool:
-        pl = MaxPool2D((2,2))(actvB)
-        return actvB, pl
+    if dilation:
+        if filters>=32:
+            x = Conv2D(filters, 2, padding="same",dilation_rate=(1,1),kernel_initializer=initializer)(inputs)
+            x = BatchNormalization()(x)
+            x = Activation("relu")(x)
+            x = Conv2D(filters, 3, padding="same",dilation_rate=(2,2),kernel_initializer=initializer)(x)
+            x = BatchNormalization()(x)
+            x = Activation("relu")(x)
+            x = Conv2D(filters, 3, padding="same",dilation_rate=(3,3),kernel_initializer=initializer)(x)
+            x = BatchNormalization()(x)
+            x = Activation("relu")(x)
+        else:
+            x = Conv2D(filters, 2, padding="same",dilation_rate=(1,1),kernel_initializer=initializer)(inputs)
+            x = BatchNormalization()(x)
+            x = Activation("relu")(x)
+            x = Conv2D(filters, 3, padding="same",dilation_rate=(2,2),kernel_initializer=initializer)(x)
+            x = BatchNormalization()(x)
+            x = Activation("relu")(x)
+        
+        if pool:
+            pl = MaxPooling2D((2,2))(x)
+            if dropout:
+                x = Dropout(0.50)(x)
+            return x, pl
+        else:
+            if dropout:
+                x = Dropout(0.50)(x)
+            return x
+
     else:
-        return actvB
+        y = Conv2D(filters, 2, padding="same",kernel_initializer=initializer)(inputs)
+        y = BatchNormalization()(y)
+        y = Activation("relu")(y)
+        y = Conv2D(filters, 3, padding="same",kernel_initializer=initializer)(y)
+        y = BatchNormalization()(y)
+        y = Activation("relu")(y)
+        
+        if pool:
+            pool_ = MaxPooling2D((2,2))(y)
+            if dropout:
+                y = Dropout(0.50)(y)
+            return y, pool_
+        else:
+            if dropout:
+                y = Dropout(0.50)(y)
+            return y
 
-def createDecoderBlock(inputlayer, concatlayer, filters):
+def createDecoderBlock(inputlayer, concatlayer, filters, dropout=False, dilation=False):
     u1 = UpSampling2D((2, 2), interpolation="bilinear")(inputlayer) # "nearest", "bilinear", "bicubic"
-    c1 = Concatenate()([u1, concatlayer])
-    conv_block = createConvolutionBlock(c1, filters, pool=False)
+    actv = Activation("relu")(u1)
+    c1 = Concatenate()([actv, concatlayer])
+    conv_block2 = createConvolutionBlock(c1, filters, pool=False, dropout=dropout,dilation=dilation)
 
-    return conv_block
+    return conv_block2
+
+
+
 
 def build_model_architecture(shape, num_classes):
     inputs = Input(shape)
 
-    actv1, pl1 = createConvolutionBlock(inputs, 16, pool=True)
-    actv2, pl2 = createConvolutionBlock(pl1, 32, pool=True)
-    actv3, pl3 = createConvolutionBlock(pl2, 48, pool=True)
-    actv4, pl4 = createConvolutionBlock(pl3, 64, pool=True)
+    actv2, pl2 = createConvolutionBlock(inputs, 32, pool=True, dropout=False, dilation=True)
+    actv3, pl3 = createConvolutionBlock(pl2, 64, pool=True, dropout=True, dilation=True)
+    actv4, pl4 = createConvolutionBlock(pl3, 128, pool=True, dropout=False, dilation=True)
+    actv5, pl5 = createConvolutionBlock(pl4, 256, pool=True, dropout=True, dilation=True)
+    actv6, pl6 = createConvolutionBlock(pl5, 512, pool=True, dropout=False, dilation=True)
 
-    bridge = createConvolutionBlock(pl4, 128, pool=False)
 
-    dec1 = createDecoderBlock(bridge, actv4, 64)
-    dec2 = createDecoderBlock(dec1, actv3, 48)
-    dec3 = createDecoderBlock(dec2, actv2, 32)
-    dec4 = createDecoderBlock(dec3, actv1, 16)
+    bridge = createConvolutionBlock(pl6, 1024, pool=False, dropout=False, dilation=False)
 
-    output = Conv2D(num_classes, 1, padding="same", activation="softmax")(dec4)
+
+    dec1 = createDecoderBlock(bridge, actv6, 512, dropout=False, dilation=False)
+    dec2 = createDecoderBlock(dec1, actv5, 256, dropout=True, dilation=False)
+    dec3 = createDecoderBlock(dec2, actv4, 128, dropout=False, dilation=False)
+    dec4 = createDecoderBlock(dec3, actv3, 64, dropout=True, dilation=False)
+    dec5 = createDecoderBlock(dec4, actv2, 32, dropout=False, dilation=False)
+
+    output = Conv2D(num_classes, 1, padding="same", activation="softmax")(dec5)
 
     return Model(inputs, output)
 
@@ -108,18 +148,29 @@ def load_image_test(datapoint: dict) -> tuple:
 
     return input_image, input_mask
 
+
+def data_augmentation(input_image, input_mask):
+    input_image = tf.image.random_brightness(input_image, max_delta=0.1)
+    input_image = tf.image.random_contrast(input_image, lower=0.1, upper=0.2)
+    input_image = tf.image.random_flip_left_right(input_image)
+
+    return input_image, input_mask
+
 def load_dataset(dataset_path,training_data):
     AUTOTUNE = tf.data.experimental.AUTOTUNE
-    BUFFER_SIZE = 1000
+    BUFFER_SIZE = 1024
+    
     train_dataset = tf.data.Dataset.list_files(dataset_path + training_data + "*.jpg", seed=42)
     train_dataset = train_dataset.map(parse_image)
-
+    
     val_dataset = tf.data.Dataset.list_files(dataset_path + val_data + "*.jpg", seed=42)
     val_dataset =val_dataset.map(parse_image)
 
     dataset = {"train": train_dataset, "val": val_dataset}
-    dataset['train'] = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    
+    dataset['train'] = dataset['train'].map(load_image_train, num_parallel_calls=AUTOTUNE)
     dataset['train'] = dataset['train'].shuffle(buffer_size=BUFFER_SIZE, seed=42)
+    dataset['train'] = dataset['train'].map(data_augmentation, num_parallel_calls=AUTOTUNE)
     dataset['train'] = dataset['train'].repeat()
     dataset['train'] = dataset['train'].batch(batch_size)
     dataset['train'] = dataset['train'].prefetch(buffer_size=AUTOTUNE)
@@ -128,7 +179,6 @@ def load_dataset(dataset_path,training_data):
     dataset['val'] = dataset['val'].repeat()
     dataset['val'] = dataset['val'].batch(batch_size)
     dataset['val'] = dataset['val'].prefetch(buffer_size=AUTOTUNE)
-
 
     return dataset
 
@@ -157,12 +207,20 @@ model = build_model_architecture(input_size, N_CLASSES)
 
 loss = tf.keras.losses.SparseCategoricalCrossentropy()
 
-model.compile(optimizer=tf.keras.optimizers.Adam(1e-4), loss = loss, metrics=['accuracy'])
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+
+model.compile(optimizer=optimizer, loss = loss, metrics=['accuracy'])
+
+
+#model.summary()
+
+#$ tensorboard --log_dir=logs/
 
 callbacks = [
             ModelCheckpoint("pretreinedmodels/model.val_loss={val_loss:.5f}.h5", monitor='val_loss', verbose=1, save_best_model=True),
-            ReduceLROnPlateau(monitor="val_loss", patience=3, factor=0.1, verbose=1, min_lr=1e-6),
-            EarlyStopping(monitor="val_loss", patience=5, verbose=1)
+            EarlyStopping(monitor="val_loss", patience=5, verbose=1),
+            TensorBoard(log_dir="logs/{}".format(time()))
         ]
 
 model.fit(dataset['train'],
@@ -173,4 +231,3 @@ model.fit(dataset['train'],
             verbose=1,
             callbacks=callbacks
         )
- 
